92a16e716640b308d0b1eebc5e51767f81773c49
Dawid Kaczybura <kaczybura.dawid@gmail.com>
2021-08-08 18:43:25 +0200

    example2

---
+++ Example.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Example.hpp"
+
+int main(){
+    std::cout << "ll" << std::endl;
+}
---
+++ test/ExampleTest.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "../Example.hpp"
+
+TEST(ExampleTest, lololo){
+    EXPECT_TRUE(false);
+    EXPECT_TRUE(true);
+} 
